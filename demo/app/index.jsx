c00e608b91df13c2ce8c0ae2fd43c38fdace2d81
Texas Holdem Poker Tables <39344302+poserjs@users.noreply.github.com>
2025-08-13 22:25:16 -0700

    chore: move sample CSV data to demo fixtures

--- demo/app/index.jsx
+++ demo/app/index.jsx
@@ -1,15 +1,12 @@
-  "use client";
-  import React from "react";
-  import ReactTableCSV from "@/components/ReactTableCsv";
+"use client";
+import React from "react";
+import ReactTableCSV from "@/components/ReactTableCsv";
+import { shortSampleCSV } from "./sample-data";
 
-  const sampleCSV = `Name,Department,Position
-  John Smith,Engineering,Senior Developer
-  Sarah Johnson,Marketing,Manager`;
-
-  export default function Page() {
-    return (
-      <main className="p-6">
-        <ReactTableCSV csvString={sampleCSV} />
-      </main>
-    );
-  }
+export default function Page() {
+  return (
+    <main className="p-6">
+      <ReactTableCSV csvString={shortSampleCSV} />
+    </main>
+  );
+}
